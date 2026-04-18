95c4433d9ea1299597a8d34c2b4dcaf387d9ca39
Tamachamo <reiji.shiokawa@gmail.com>
2026-04-18 21:38:01 +0900

    route.ts の更新

--- app/api/gas/route.ts
+++ app/api/gas/route.ts
@@ -1,8 +1,8 @@
 import { NextResponse } from 'next/server';
 
 export async function GET() {
-  // 💡 いただいた新しいGASのURLに差し替えています
-  const GAS_API_URL = 'https://script.google.com/macros/s/AKfycbyaP87SP53XFoEJaRrUaDIO34r__K9Jm5j-lyet84e-EoUOi7lysK8cghE3kQwign0U/exec';
+  // 最新のGASのURL
+  const GAS_API_URL = 'https://script.google.com/macros/s/AKfycbyC9KKVwsKK83xOZ97yIcu_YFn3NrI6CEqoirBLyeiX4ocqXTRU8UcznENSvJjYg-HC/exec';
 
   try {
     const res = await fetch(GAS_API_URL, { 
