d3ad237449a3b46289d3318f023b8f4db201320f
Tamachamo <reiji.shiokawa@gmail.com>
2026-04-18 22:58:40 +0900

    next.config.mjs の更新

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // TypeScriptの型エラーがあっても、強制的にビルドを完了させる設定
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  // ESLintの文法エラーがあっても、強制的にビルドを完了させる設定
-  eslint: {
-    ignoreDuringBuilds: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'unite-db.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
   },
 };
 
 export default nextConfig;
-
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ['unite-db.com'],
-  },
-}
-
-module.exports = nextConfig
